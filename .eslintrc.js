79d19393bbbf22dd435e30f7b6bfa603d7fbab46
Edwin Cromley <cromleyedwin89@gmail.com>
2020-01-06 22:30:08 -0500

    Theme: More adjustments to files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,14 @@
 module.exports = {
-	root: true,
-	extends: [ 'wpcalypso/react', 'plugin:jest/recommended', 'prettier', 'prettier/react' ],
-	parser: 'babel-eslint',
-	env: {
-		browser: true,
-		'jest/globals': true,
-		node: true,
-	},
-	plugins: [ 'jest', 'import', 'prettier' ],
+	extends: '@10up/eslint-config',
+	plugins: ['react'],
 	rules: {
-		// REST API objects include underscores
-		camelcase: 0,
-
-		'wpcalypso/import-docblock': 0,
-		'wpcalypso/jsx-classname-namespace': 0,
-		'jest/no-disabled-tests': 0,
-
-		'import/no-extraneous-dependencies': [ 'error', { packageDir: './' } ],
-		'prettier/prettier': 'warn',
+		'react/jsx-uses-react': 'error',
+		'react/jsx-uses-vars': 'error',
+	},
+	globals: {
+		module: true,
+		process: true,
+		wp: true,
+		lodash: true,
 	},
 };
