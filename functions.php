a0941ecead213d527f693546e11eb55344ffdef1
Edwin Cromley <cromleyedwin89@gmail.com>
2020-01-06 21:10:16 -0500

    Theme: Change text to be replaceable via CLI tool.

--- functions.php
+++ functions.php
@@ -2,24 +2,24 @@
 /**
  * WP Theme constants and setup functions
  *
- * @package Eri
+ * @package EriScaffold
  */
 
 // Useful global constants.
-define( 'ERI_VERSION', '0.1.0' );
-define( 'ERI_TEMPLATE_URL', get_template_directory_uri() );
-define( 'ERI_PATH', get_template_directory() . '/' );
-define( 'ERI_INC', ERI_PATH . 'includes/' );
+define( 'ERI_SCAFFOLD_VERSION', '0.1.0' );
+define( 'ERI_SCAFFOLD_TEMPLATE_URL', get_template_directory_uri() );
+define( 'ERI_SCAFFOLD_PATH', get_template_directory() . '/' );
+define( 'ERI_SCAFFOLD_INC', ERI_SCAFFOLD_PATH . 'includes/' );
 
-require_once ERI_INC . 'core.php';
-require_once ERI_INC . 'overrides.php';
-require_once ERI_INC . 'template-tags.php';
-require_once ERI_INC . 'utility.php';
-require_once ERI_INC . 'blocks.php';
+require_once ERI_SCAFFOLD_INC . 'core.php';
+require_once ERI_SCAFFOLD_INC . 'overrides.php';
+require_once ERI_SCAFFOLD_INC . 'template-tags.php';
+require_once ERI_SCAFFOLD_INC . 'utility.php';
+require_once ERI_SCAFFOLD_INC . 'blocks.php';
 
 // Run the setup functions.
-Eri\Core\setup();
-Eri\Blocks\setup();
+EriScaffold\Core\setup();
+EriScaffold\Blocks\setup();
 
 // Require Composer autoloader if it exists.
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
